84a7d30f805396d9cf8f7c038042963c9bf29145
mzw86020424 <mzw86020424@gmail.com>
2021-03-11 09:37:14 +0900

    モーダルのdivを作成

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -8,6 +8,20 @@ class Lesson extends React.Component{
           <p>{this.props.name}</p>
           <img src={this.props.image} />
         </div>
+        
+        {/* モーダル */}
+        <div className="modal">
+          <div className="modal-inner">
+            <div className="modal-header"></div>
+            <div className="modal-introduction">
+              <h2>{this.props.name}</h2>
+              <p>{this.props.introduction}</p>
+            </div>
+            <button className="modal-close-btn">
+              とじる
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -7,18 +7,22 @@ class Main extends React.Component{
       {
         name: 'HTML & CSS',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg',
+        introduction: 'WEBページはHTML、CSSという言語によってその見た目が作られています。 実際にWEBページを作りながら学んでみましょう！',
       },
       {
         name: 'Sass',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/sass.svg',
+        introduction: 'SassはCSSをより便利に効率的にするための言語です。',
       },
       {
         name: 'JavaScript',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg',
+        introduction: 'JavaScriptはフロントエンドだけでなく、サーバーサイドまで広い可能性を持つプログラミング言語です。',
       },
       {
         name: 'React',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg',
+        introduction: 'ReactはHTMLのように、サイトの見た目をつくることができるJavaScriptのライブラリです。',
       },
     ];
 
@@ -34,8 +38,9 @@ class Main extends React.Component{
             {lessonList.map((lessonItem) => {
               return (
                 <Lesson
-                name={lessonItem.name}
-                image={lessonItem.image}
+                  name={lessonItem.name}
+                  image={lessonItem.image}
+                  introduction={lessonItem.introduction}
                 />
               );
             })}
