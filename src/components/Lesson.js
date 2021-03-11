598d2db158ed93417da61909cb9b9312d0a1c3d9
mzw86020424 <mzw86020424@gmail.com>
2021-03-11 09:29:45 +0900

    mapで各コンポーネントを表示

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -3,9 +3,11 @@ import React from 'react';
 class Lesson extends React.Component{
   render() {
     return (
-      <div>
-        <p>{this.props.name}</p>
-        <img src={this.props.image} />
+      <div className="lesson-card">
+        <div className="lesson-item">
+          <p>{this.props.name}</p>
+          <img src={this.props.image} />
+        </div>
       </div>
     );
   }
